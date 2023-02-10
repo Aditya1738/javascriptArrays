18a2d7cc024fbbae8a01b0102792a30f9c31fa0b
Aditya1738 <110099468+Aditya1738@users.noreply.github.com>
2023-02-10 18:59:45 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,8 @@
+document.body.style.backgroundColor  = "skyblue";                      // background color of whole HTML element.
+document.body.style.border  = 'solid';                                 // Added border to body
+document.getElementById("p1").innerHTML = 'For Introducing me to JS';  // Added p1 text   
+document.getElementById('p1').style.fontSize = '200%';                 // size of p1 paragaph text
+document.getElementById('p1').style.color = 'yellow';                  // color to text
+document.getElementById('intro').style.fontSize = '250%';              // size of intro paragraph
+document.getElementById('intro').style.color = 'red';                  // Added color to text
+document.getElementById('p-body').style.backgroundColor = 'violet';    // background-color to whole paragraph 
